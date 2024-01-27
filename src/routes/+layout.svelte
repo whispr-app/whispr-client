5312130616b20b257dc455c3a1857c48184dd72b
Morgan Dilling <morgandilling171@gmail.com>
2024-01-27 12:11:06 +0000

    fix: Change location of main font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -47,8 +47,8 @@
 	@font-face {
 		font-family: 'Mona Sans';
 		src:
-			url('Mona-Sans.woff2') format('woff2 supports variations'),
-			url('Mona-Sans.woff2') format('woff2-variations');
+			url('$lib/Mona-Sans.woff2') format('woff2 supports variations'),
+			url('$lib/Mona-Sans.woff2') format('woff2-variations');
 		font-weight: 200 900;
 		font-stretch: 75% 125%;
 	}
